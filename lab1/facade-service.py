8808a5094176df163c0fcc069e88a0ea9015b984
IschenkoArtem31 <168985524+IschenkoArtem31@users.noreply.github.com>
2024-05-05 20:12:20 +0300

    Update facade-service.py

--- lab1/facade-service.py
+++ lab1/facade-service.py
@@ -1,1 +1,15 @@
+from flask import Flask
+
+app = Flask(__ischenko__)
+
+@app.route('/post_message', methods=['POST'])
+def post_message():
+    return 'Received POST request'
+
+@app.route('/get_messages', methods=['GET'])
+def get_messages():
+    return 'Received GET request'
+
+if __ischenko__ == '__main__':
+    app.run()
 
