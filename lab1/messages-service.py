c1e7b49067bd57cf678fe8152f75d33a035bc868
IschenkoArtem31 <168985524+IschenkoArtem31@users.noreply.github.com>
2024-05-05 20:14:02 +0300

    Update messages-service.py

--- lab1/messages-service.py
+++ lab1/messages-service.py
@@ -1,1 +1,11 @@
+from flask import Flask
+
+app = Flask(__ischenko__)
+
+@app.route('/get_message', methods=['GET'])
+def get_message():
+    return 'not implemented yet'
+
+if __ischenko__ == '__main__':
+    app.run()
 
