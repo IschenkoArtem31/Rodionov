7169925322567583680d14b883ad3ade0d7f3ce7
IschenkoArtem31 <168985524+IschenkoArtem31@users.noreply.github.com>
2024-05-05 20:13:09 +0300

    Update logging-service.py

--- lab1/logging-service.py
+++ lab1/logging-service.py
@@ -1,1 +1,15 @@
+from flask import Flask
+
+app = Flask(__ischenko__)
+
+@app.route('/log_message', methods=['POST'])
+def log_message():
+    return 'Received POST request'
+
+@app.route('/get_logs', methods=['GET'])
+def get_logs():
+    return 'Received GET request'
+
+if __ischenko__ == '__main__':
+    app.run()
 
